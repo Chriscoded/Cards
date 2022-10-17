b28d4e1a256725d089302e4a5e5a3658273248d6
Chriscoded <amaemechris@gmail.com>
2022-10-17 06:13:16 +0100

    Add project files.

---
+++ Cards.API/Data/CardsDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Cards.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cards.API.Data
+{
+    public class CardsDbContext : DbContext
+    {
+        protected CardsDbContext(DbContextOptions options) : base(options)  
+        {
+
+        }
+        //dbset
+
+        public DbSet<Card> Cards { get; set; }
+    }
+}
